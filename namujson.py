504048201beaa29a5a562eff0315b1a372b2be3b
2DU <min08101@naver.com>
2017-07-15 02:21:40 +0900

    수정

--- namujson.py
+++ namujson.py
@@ -16,23 +16,20 @@ curs = conn.cursor(pymysql.cursors.DictCursor)
 def isNumber(data):
   try:
     tmp1 = float(data)
-    return True
+    
+    return(True)
   except ValueError:
-    return False
+    return(False)
 
 # 편집자를 구분하는 부분입니다. 리그베다 위키 유저는 R:로, 나무위키 유저는 N:의 Prefix가 붙습니다.
 def editorProcess(editor):
-    if editor.find("R:") != -1:
-        pass
-    elif isNumber(editor) == True:
+    if(editor.find("R : ") != -1 or isNumber(editor) == True):
         pass
     else:
-        editor = "N:"+editor
-    return editor
+        editor = "N : " + editor
     
-
-
-
+    return(editor)
+    
 def mainprocess(dictdata):
     revision = 0
     revisionNum = 0
@@ -44,8 +41,10 @@ def mainprocess(dictdata):
         try:
             # 데이터를 읽어서 본문, 문서 제목, 리비전 수를 셉니다.
             print(i)
+            
             revision = len(dictdata[i]['contributors'])
             namespace = str(dictdata[i]['namespace'])
+            
             if(namespace == '0' or namespace == '1'):
                 if(namespace == '1'):
                     text = re.sub("\[\[분류:(?P<in>(?:(?!]]).)*)]]", "{{{#!noin [[분류:\g<in>]]}}}", str(dictdata[i]['text']))
@@ -53,12 +52,14 @@ def mainprocess(dictdata):
                 else:
                     text = str(dictdata[i]['text'])
                     title = str(dictdata[i]['title'])
+                    
                 # SQL에 삽입 합니다.
                 try:
                     curs.execute("insert into data (title, data, acl) value ('" + pymysql.escape_string(title) + "', '" + pymysql.escape_string(text) + "', '')")
                     conn.commit()
                 except:
                     Errorlist.append(title)
+                    
                 # 이하의 코드는 리비전별로 실행됩니다.
                 for x in range(revision):      
                     revisionNum = x+1
@@ -67,35 +68,39 @@ def mainprocess(dictdata):
                     editor = editorProcess(editor)
                     try:
                         curs.execute("insert into history (id, title, data, date, ip, send, leng) value ('" + pymysql.escape_string(str(revisionNum)) + "', '" + pymysql.escape_string(title) + "', '', '" + pymysql.escape_string(editTime) + "', '" + pymysql.escape_string(editor) + "', 'NamuWiki and RigVeda', '0')")
+                        
                         conn.commit()
                     except:
                         Errorlist.append(title)
+                        
         except OSError as error:
             print(error)
             Errorlist.append(title)
+            
     print("문서 변환 작업이 종료되었습니다.")
-    print(str(len(dictdata))+"개의 문서가 데이터에 존재합니다. 그 중 "+str((len(dictdata)-len(Errorlist)))+"개의 문서가 변환되었습니다. 오류가 발생한 문서는 "+str(len(Errorlist))+" 개 입니다.")
-
-
+    print(str(len(dictdata)) + "개의 문서가 데이터에 존재합니다. 그 중 " + str((len(dictdata) - len(Errorlist))) + "개의 문서가 변환되었습니다. 오류가 발생한 문서는 " + str(len(Errorlist)) + " 개 입니다.")
 
 print("이 스크립트는 나무위키 JSON 데이터가 필요합니다. 데이터를 로딩합니다.\n만약 이 스크립트를 이전에 실행한 적이 있으시다면, 그때 생성된 임시 파일을 사용합니다.")
 
-if os.path.exists(os.path.join("rawdata.pickle")) == True:
+if(os.path.exists(os.path.join("rawdata.pickle")) == True):
     print("임시 파일이 로딩되었습니다.")
     rawdata_address = r"rawdata.pickle"
     rawdata = open(os.path.join(rawdata_address),'rb')
     dictdata = pickle.load(rawdata)
-else :
+else:
     print("임시 파일이 없으므로 JSON을 로딩합니다.")
+    
     jsondata = os.path.join('namuwikidata.json')
     namuwikidata = open(jsondata,'r')
     print("JSON 데이터 읽기 완료")
+    
     dictdata = json.load(namuwikidata)
     namuwikidata.close()
     print("JSON 데이터 사전형으로 변환 완료")
-    tempdata = open('rawdata.pickle','wb')
-    pickle.dump(dictdata,tempdata)
-    print("다음 실행을 위해서 임시 데이터를 저장합니다.")
+    
+#    tempdata = open('rawdata.pickle','wb')
+#    pickle.dump(dictdata,tempdata)
+#    print("다음 실행을 위해서 임시 데이터를 저장합니다.")
     
 print("모든 사전 작업이 종료되었습니다. 변환을 시작합니다.")
 mainprocess(dictdata)
